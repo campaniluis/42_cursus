673894b1757eba696c1ce5e38f0491ca1c3246d2
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 15:34:23 +0100

    Create ft_strlcat.c

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,21 @@
+strlcat (char *src, char *dest, int buffer)
+{
+  int index;
+  int counter;
+  
+  index = '0';
+  counter = '0';
+  
+  while (dest[counter] != '\0')
+    counter++;
+ 
+  while (counter <= buffer)
+  {
+    src[counter] = dest[index];
+    index++;
+    counter++;
+  }
+    
+// Returns size of created string
+return (counter);
+}
