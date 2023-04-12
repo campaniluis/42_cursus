4c8d76278598d27b12b65fc2ea22b9d762f288fd
Luis Claudio Barnech Campani Farias <lclaudio@c2r11s2.42lisboa.com>
2023-04-12 16:11:29 +0100

    Feat: added headers

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,4 +1,16 @@
-bzero (void *ptr, size_t num)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 16:02:04 by lclaudio          #+#    #+#             */
+/*   Updated: 2023/04/12 16:08:13 by lclaudio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *ft_bzero (void *ptr, int num)
 {
   int index;
   int limit;
@@ -6,7 +18,6 @@ bzero (void *ptr, size_t num)
   index = 0;
   limit = num;
   // First step: point to starting address
-  
   // Second: set each byte in the block with 0's until the specified value
     while (index <= limit)
     {
