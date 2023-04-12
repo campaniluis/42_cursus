4134ece5f0de7ac8cc1c14fcfedd87a0a48e16ee
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 13:41:11 +0100

    Create ft_memcpy.c

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,17 @@
+ft_memcpy (void *dest, const void *src, size_t num)
+{
+  int index;
+  int limit; 
+  
+  // First step: point to starting address
+  index = 0;
+  limit = num;
+  
+  // Second: set each byte in the block until the specified value
+    while (value != '\0' && index <= limit)
+    {
+      src[index] == dest[index];
+      index++;
+    }
+  return(0);
+}
