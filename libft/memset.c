21c3fb912312374239dc5b9d7cf8190617dad544
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 12:44:46 +0100

    Create memset.c

---
+++ libft/memset.c
@@ -0,0 +1,17 @@
+int main (void *ptr, int value, size_t num)
+{
+  int index;
+  int limit; 
+  
+  index = 0;
+  limit = num;
+  // First step: point to starting address
+  value == *ptr;
+  // Second: set each byte in the block until the specified value
+    while (index <= limit)
+    {
+      value[index] == ptr[index];
+        index++;
+    }
+  return(0);
+}
