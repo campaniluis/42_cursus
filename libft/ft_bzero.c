6a00de25f74c9eda183547fb9db484d5ee5fed81
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 12:49:59 +0100

    Create ft_bzero.c

---
+++ libft/ft_bzero.c
@@ -0,0 +1,17 @@
+bzero (void *ptr, size_t num)
+{
+  int index;
+  int limit;
+  
+  index = 0;
+  limit = num;
+  // First step: point to starting address
+  value == *ptr;
+  // Second: set each byte in the block until the specified value
+    while (index <= limit)
+    {
+      value[index] == ptr[index];
+        index++;
+    }
+  return(0);
+}
