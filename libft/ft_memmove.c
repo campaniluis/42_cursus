d70427246763c081c92b4ba647972a6c5c1b5bc7
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 15:15:54 +0100

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,1 +1,17 @@
-
+ft_memmove (void *dest, const void *src, size_t num)
+{
+  int index;
+  int limit;
+  
+  // First step: point to starting address
+  index = 0;
+  limit = num;
+  
+  // Second: set each byte in the block until the specified value
+    while (index <= limit)
+    {
+      src[index] == dest[index];
+      index++;
+    }
+  return(0);
+}
