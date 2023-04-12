29ed49f41b7b0489c7f8aeb91f6c6a95c4c23eed
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-12 15:45:08 +0100

    Update ft_toupper.c

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -2,7 +2,7 @@ ft_toupper (char lowercase)
 {
   char uppercase;
 
-  uppercase = (lowercase - 22);
+  uppercase = (lowercase - 32);
     if  (lowercase >= 97 && lowercase <= 122)
       return(uppercase);
 }
