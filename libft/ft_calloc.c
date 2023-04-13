efda1a29fcc502792d1a8cd47117aae66736cc40
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-13 08:29:53 +0100

    Create ft_calloc.c

---
+++ libft/ft_calloc.c
@@ -0,0 +1,13 @@
+
+// malloc ok!!
+
+// malloc + initializing all bytes to zero
+ft_calloc (int num, sizeof(size))
+{
+	
+	malloc 
+	// num * sizeof = total_size
+	// requesting total_size from the operating system
+	// if allocation fails
+	return ('\0');
+}
