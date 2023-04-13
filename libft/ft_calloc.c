54879a814420450d2508b08915188d4761df7236
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-13 12:03:23 +0100

    Update ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -13,13 +13,13 @@
 char	*ft_calloc (int num, sizeof(size))
 {
 	int	index;
-	char *total_size
+	char *total_size;
 	
 	index = 0;
-	*total_size = malloc * (num * sizeof(size))
+	*total_size = (char*)malloc * (num * sizeof(size))
 	while (index < num)
 	{
-		total_size[index] = '0';
+		total_size[index] = 0;
 		index++;
 	}
 	return (*total_size);
