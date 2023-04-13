17dae0a1745b3bf32e0ab0d5285f780d8afa893c
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-13 08:57:47 +0100

    Create ft_memcmp.c

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,9 @@
+int ft_memcmp (char *ptr1,char *ptr2, int num)
+{
+	int index;
+	
+	index = 0;
+	while (index <= num && ptr1[index] == ptr2[index])
+		index++;
+	return(ptr1[index] - ptr2[index]);
+}
