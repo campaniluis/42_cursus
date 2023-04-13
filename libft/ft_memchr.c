3df68d090e956a9da1fe0bb82ccae854e77bc74f
= <=>
2023-04-13 09:40:00 +0100

    Feat: headers + ft_strnstr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 09:38:41 by lclaudio          #+#    #+#             */
+/*   Updated: 2023/04/13 09:38:43 by lclaudio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 // search for a specific byte value within a block of memory
 ft_memchr (void *ptr, int value, int num)
 {
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,4 +1,35 @@
-char *ft_strnstr ()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 09:37:55 by lclaudio          #+#    #+#             */
+/*   Updated: 2023/04/13 09:37:58 by lclaudio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *ft_strnstr (char *haystack, char *needle, int num)
 {
-    
+	int	index;
+	int	n_size;
+	
+	index = 0;
+	n_size = 0;
+	
+	while (needle[n_size] != '\0')
+		n_size++;
+
+	while ((needle[index] || haystack[index]) != '\0' && index < num)
+	{	
+		if (needle[index] == haystack[index])
+		{
+			while(needle[index] == haystack[index])
+				index++;
+			return (haystack[index - n_size]);
+		}
+		index++;
+	}
+	return (0);
 }
