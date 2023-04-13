95734f2e21fef702ae6d3d16257e10d9dbc600ea
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-13 08:36:43 +0100

    Create ft_strncmp.c

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,10 @@
+ft_strncmp (char *str1, *char str2, int num)
+{
+	int index;
+	
+	index = 0;
+	while (index <= num && str1[index] == str2[index])
+		index++;
+	// returns int of result comparison
+	return (str1[index] - str2[index]);
+}
