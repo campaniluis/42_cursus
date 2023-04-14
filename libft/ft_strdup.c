8f878eb8bd001d20579ff05ef364f935ccf90154
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-14 10:46:24 +0100

    Create ft_strdup.c

---
+++ libft/ft_strdup.c
@@ -0,0 +1,22 @@
+// duplicate of a string
+// allocating memory for the duplicate string
+char *ft_strdup (char *str)
+{
+	int index;
+	int counter;
+	char *new_str;
+
+	index = 0;
+	counter = 0;
+	while (str[counter] != '\0')
+		  counter++;
+	
+	new_str = (char *)malloc(counter + 1);	
+	while (index < counter)
+	{
+		new_str[index] = str[index];
+		index++;
+	}
+		new_str[index] = '\0';
+		return(new_str);
+}
