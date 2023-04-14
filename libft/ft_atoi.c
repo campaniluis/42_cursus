9816c9fa07a36fe04b5dac3aa0f11f5c6a8fc4ba
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-14 17:04:25 +0100

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,15 +1,11 @@
 int ft_atoi (char *str)
 {
-	int length;
  	int	index;
 	int	output;
 	
 	index = 0;
-	length = 0;
 	output = 0;
-	while (str[length] != '\0')
-		length++;
-	while (index < length)
+	while (str[index] != '\0')
 	{
 		output = output * 10 + (str[index] - '0')		
 		index++;
