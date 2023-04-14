050fc88d37f4ba5de3f79acf8f0ff31539973557
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-14 16:29:53 +0100

    Create ft_strtrim.c

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,37 @@
+char *ft_strtrim(char const *s1, char const *set)
+{
+	char *output;
+	int	count_set;
+	int index;
+	int find_set;
+	int s1_len;
+	
+	count_set = 0;
+	index = 0;
+	find_set = 0;
+	s1_len = 0;
+	while (s1[s1_len] != '\0')
+		s1_len++;
+	while (set[count_set] != '\0')
+		count_set++;
+	output = (char *)malloc(s1_len - count_set + 1)
+	while (index < s1_len)
+	{
+		if (s1[index] == set[0])
+		{
+			while(find_set < count_set)
+			{
+				if s1[index + find_set] == set[find_set]
+					find_set++;
+				else
+			// than it is not the searched string
+					break;
+			}
+			// if it is, remove it from s1
+			index = index + count_set;
+		}
+		output [index] = s1[index];
+	}
+	output[s1_len - count_set] = '\0';
+	return(output);
+}
