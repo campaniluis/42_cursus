8e448862c577fea672c35bfaeaec1f4e4956441e
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-16 10:48:08 +0100

    Update and rename ft_split to ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -2,9 +2,14 @@ char **ft_split(char const *s, char c)
 {
 	char **output;
 	int	index;
+	int	counter;
 	
-	char **output[]
 	index = 0;
+	counter = 0;
+	
+	while 
+	char *output[index] = (char *)malloc()
+	
 	while word increment
 	if c create a new string
 	
