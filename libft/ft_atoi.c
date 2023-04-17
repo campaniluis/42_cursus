d9623ddc7058cc41b325a30be99d309cff90aa65
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-17 08:41:11 +0100

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -7,8 +7,28 @@ int ft_atoi (char *str)
 	output = 0;
 	while (str[index] != '\0')
 	{
-		output = output * 10 + (str[index] - '0')		
-		index++;
+		if (str[index] == ' ')
+			index++;
+		else if (str[index] == '-')
+		{
+			output = (output * -1);
+			index++;
+		}
+		else if (str[index] == '+')
+			index++;
+		if (str[index] == '0')
+			index++;
+		else if (str[index] >= '1' && str[index] <= '9')
+		{
+			while (str[index] >= '0' && str[index] <= '9')	
+			{
+				output = output * 10 + (str[index] - '0');
+				index++;
+			}
+			break;
+		}
+	else
+		return(0);
 	}
 	return (output);
 }
