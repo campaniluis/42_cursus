784e7f2709e956598567c9dac912bd179527d208
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-18 09:49:53 +0100

    Update ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,4 +1,22 @@
+// apply a function to each character in a string and create a new string with the results
+// malloc ok
+
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	int		index;
+	int		s_len;
+	char	*output;
 	
+	index = 0;
+	s_len = ft_strlen(s);
+	output = (char *)malloc(s_len + 1);
+	if(output == '\0' || s == '\0')
+		return('\0');
+	while(index < s_len)
+	{
+		output[index] = f(index, s[index]);
+		index++;
+	}
+	output[index] = '\0';
+	return(output);
 }
