4d6c2bff5e85df2a2323ea35bad98d92b0f2a64c
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-18 10:36:51 +0100

    Update ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,6 +1,5 @@
 char *ft_strtrim(char const *s1, char const *set)
 {
-	
 	int		frst_trg_index;
 	int		last_trg_index;
 	char	*output;
@@ -9,10 +8,11 @@ char *ft_strtrim(char const *s1, char const *set)
 	last_trg_index = ft_strlen(s1) -1;
 	while(s1[frst_trg_index]== *set)
 		frst_trg_index++;
-	while(s1[last_trg_index]== *set)
+	while(s1[last_trg_index]== *set && last_trg_index > frst_trg_index)
 		last_trg_index--;
 	output = (char*)malloc(last_trg_index - frst_trg_index + 2);
-	index = 0;
+	if (output == '\0')
+		return ('\0');
 	while (index <= last_trg_index - frst_trg_index)
 	{
 		output[index] = s1[index + frst_trg_index];
