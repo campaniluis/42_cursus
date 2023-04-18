a4beaf90bd5c16308bdcadf730158c9cbddd6ca7
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-18 10:29:51 +0100

    Update ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,48 +1,23 @@
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *output;
-	int	count_set;
-	int index;
-	int find_set;
-	int s1_len;
 	
-	count_set = 0;
+	int		frst_trg_index;
+	int		last_trg_index;
+	char	*output;
+
+	frst_trg_index = 0;
+	last_trg_index = ft_strlen(s1) -1;
+	while(s1[frst_trg_index]== *set)
+		frst_trg_index++;
+	while(s1[last_trg_index]== *set)
+		last_trg_index--;
+	output = (char*)malloc(last_trg_index - frst_trg_index + 2);
 	index = 0;
-	find_set = 0;
-	s1_len = 0;
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (set[count_set] != '\0')
-		count_set++;
-	if (s1 == '\0')
-		return(0);
-	else if (s1_len <= count_set)
-	// return new empty string
-		return(); 
-	else
+	while (index <= last_trg_index - frst_trg_index)
 	{
-		output = (char *)malloc(s1_len - count_set + 1);
-		while (index < s1_len)
-		{
-			if (s1[index] == set[0])
-			{
-				while(find_set < count_set)
-				{
-					if (s1[index + find_set] == set[find_set])
-						find_set++;
-					else
-					{
-						find_set = 0;
-						break;
-					}
-				// than it is not the searched string
-				}
-				// if it is, remove it from s1
-				index = index + count_set;
-			}
-			output [index] = s1[index];
-		}
-		output[s1_len - count_set] = '\0';
-		return(output);
+		output[index] = s1[index + frst_trg_index];
+		index++;
 	}
+	output[index] = '\0';
+	return(output);
 }
