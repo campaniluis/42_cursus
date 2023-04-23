5976f3801947b4611af0e999cd592925c9b1b37f
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-23 10:19:21 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -27,11 +27,11 @@ struct s_list *next;
 int		ft_atoi (char *str);
 void	ft_bzero(void *ptr, size_t limit)
 // char	*ft_calloc (int num, sizeof(size));
-void	ft_isalnum(char *str);
-void	ft_isalpha(char *str);
-void	ft_isascii(char *str);
-void	ft_isdigit(char *str);
-void	ft_isprint(char *str);
+int	ft_isalnum(char *str); // here changed, maybe still has to change
+int	ft_isalpha(char *str); // here changed, maybe still has to change
+int	ft_isascii(char *str); // here changed, maybe still has to change
+int	ft_isdigit(char *str); // here changed, maybe still has to change
+int	ft_isprint(char *str); // here changed a lot, maybe still has to change
 char	*ft_itoa(int n);
 void	ft_memchr (void *ptr, int value, int num);
 int		ft_memcmp (char *ptr1,char *ptr2, int num);
@@ -39,20 +39,20 @@ void	ft_memcpy (void *dest, const void *src, size_t num);
 void	ft_memmove (void *dest, const void *src, size_t num);
 // void	ft_memset (void *ptr, void value, size_t num);
 char	**ft_split(char const *s, char c);
-void	ft_strchr (char *str, char to_find);
+char	*ft_strchr (const char *str, int to_find); // here changed
 char	*ft_strdup (char *str);
-void	ft_strjoin(char *s1, char *s2);
-void	strlcat (char *src, char *dest, int buffer);
-char	*ft_strlcpy(char *origin, char *dest);
+char	*ft_strjoin(char *s1, char *s2); // here changed
+size_t	strlcat (char *src, char *dest, int buffer); // here changed
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); // here changed a lot
 int		ft_strlen(char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 // int		ft_strncmp (char *str1, *char str2, int num);
-char	*ft_strnstr (char *haystack, char *needle, int num);
-char	*ft_strrchr (char *str, char to_find);
+char	ft_strnstr (const char *haystack, const char *needle, size_t num); // here changed
+char	*ft_strrchr (const char *str, int to_find); // here changed
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_tolower (char uppercase);
-int		ft_toupper (char lowercase);
+int		ft_tolower (int uppercase); // here changed
+int		ft_toupper (int lowercase); // here changed
 
 
 // Constant definitions
