785a656b3ae43bd84757a96fe1ab082bfbb21381
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-04-27 08:15:36 +0000

    Tried to fix memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/21 08:09:50 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/04/23 16:51:30 by lclaudio         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -16,10 +5,10 @@ void	ft_memset(void *ptr, int value, size_t num)
 {
 	size_t	index;
 
-	index = 0;
-	if (num <= 0 || ptr == NULL)
+	if (num < 0 || ptr == NULL)
 		return (0);
-	while (ptr[index] != '\0' && index < num)
+	index = 0;
+	while (ptr[index] && index < num)
 	{
 		((char *)ptr)[index] = value;
 		index++;
