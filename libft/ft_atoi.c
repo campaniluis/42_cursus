a3fe17d167d3f9e3e9b898b08172d5bcd47d9887
= <=>
2023-04-30 13:42:59 +0100

    Fixed a bit atoi, not ready yet

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 08:08:52 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/04/30 11:45:55 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/04/30 13:42:26 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@ int	ft_atoi(char *str)
 {
 	int	index;
 	int	output;
+	int	is_negative;
 
 	index = 0;
 	output = 0;
-	while (ft_strchr(" 0-+", str[index]) != NULL)
+	is_negative = 0;
+	while (ft_strchr(" 0\t\v\f\r\n\f", str[index]))
+		index++;
+	while (ft_strchr("-+", str[index]))
 	{
 		if (str[index] == '-')
-			output = (output * -1);
+			is_negative = 1;
+		else
+			is_negative = 0;
 		index++;
 	}
 	while (str[index] >= '0' && str[index] <= '9')
@@ -30,7 +36,7 @@ int	ft_atoi(char *str)
 			output = output * 10 + (str[index] - '0');
 			index++;
 	}
-	if (str[index] > '9')
-		return (0);
+	if (is_negative == 1)
+		output = (output * -1);
 	return (output);
 }
