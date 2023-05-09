57005067b09fb56d82c1cf44e0d295a678b694b4
= <=>
2023-05-09 18:00:32 +0100

    Update bonus name + lstnew

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:09:56 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/05/08 10:46:48 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/05/09 17:56:22 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	size_t	content_size;
-	t_list	next;
+	t_list	*new_node;
 
-	content_size = ft_strlen((char *)content);
-	next = calloc(content, sizeof(t_list));
-	next = ft_strdup((char *)content);
-	return (next);
+	new_node = calloc(content, sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
