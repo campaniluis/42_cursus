fd873c37cbc2219706f62c47a93a5d6e147ad604
= <=>
2023-05-27 19:06:18 +0100

    Did my best to fix split for today

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 08:09:54 by lclaudio			       #+#    #+#             */
-/*   Updated: 2023/05/27 16:57:06 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/05/27 19:05:50 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,18 +149,27 @@ char	**ft_split(char const *s, char c)
 	int		index;
 	size_t	len;
 
+	printf("%s", "\nI'M INSIDE THE SPLIT\n");
 	index = 0;
 	counter = 0;
-	// error_handling(*s, c);
 	if (!s)
+	{
+		printf("%s", "\nI'M INSIDE THE if (!s)\n");
 		return (NULL);
+	}
+	if (!ft_strchr(s, c))
+	{
+		printf("%s", "\nI'M AT THE if (!ft_strchr(s, c))\n");
+		return ((char **)s);
+	}
 	cabinet = (char **)malloc((word_counter(s, c) + 1) * sizeof(char));
 	if (!cabinet)
 		return (0);
 	while (s[index])
 	{
+		printf("%s", "\nI'M GETTING IN THIS WHILE\n");
 		len = word_length(&s[index], c);
-		cabinet[counter] = (char *)malloc((len + 1)	* sizeof(char));
+		cabinet[counter] = (char *)malloc((len + 1) * sizeof(char));
 		if (!cabinet[counter])
 		{
 			free_malloc(cabinet, counter);
@@ -169,24 +178,87 @@ char	**ft_split(char const *s, char c)
 		ft_strlcpy(cabinet[counter], s, word_length(&s[index], (char)c));
 		index = (index + word_length(&s[index], c));
 		counter++;
+		index++;
 	}
-	cabinet[counter] = '\0';
+	printf("%s", "\nI'M GETTING OUT OF THIS WHILE\n");
 	return (cabinet);
 }
 
 
-int		main(void)
+// int		main(void)
+// {
+// 	char	*teste;
+// 	char	**print;
+// 	char	*vazio;
+// 	int		index;
+
+// 	teste = "post	tenebrvs	lux";
+// 	index = 0;
+// 	vazio = NULL;
+// 	printf("%s", "TESTE 1: \tNULL\tft_split(teste, ' ')\n");
+// 	print = "I'M FULL";
+// 	if (print[index] == "I'M FULL")
+// 		printf("%s", "I'M FULL");
+// 	print = ft_split(vazio, ' ');
+// 	printf("%s", "\nOUT OF THE SPLIT\n\n");
+// 	if (print == NULL)
+// 		printf("%s", "\nNULL\n");
+// 	printf("%s", "TESTE 2: \tpost	tenebrvs	lux\tft_split(teste, ' ')\n");
+// 	print = ft_split(teste, ' ');
+// 		while (index)
+// 	{
+// 		printf("%s", print[index]);
+// 		index++;
+// 	}
+// 	teste = "post tenebrvs lux";
+// 	printf("%s", "TESTE 3: \tpost tenebrvs lux\tft_split(teste, ' ')\n");
+// 	print = ft_split(teste, ' ');
+// 	while (index)
+// 	{
+// 		printf("%s", print[index]);
+// 		printf("%s", "\nHERE!!!!!!!!\n\n");
+// 		index++;
+// 	}
+// }
+
+int	main(void)
 {
-	char	*teste_1;
+	char	*teste;
 	char	**print;
+	char	*vazio;
 	int		index;
 
-	teste_1 = "post	tenebrvs	lux";
-	printf("%s", "ft_split(teste_1, ' ')\n");
-	print = ft_split(teste_1, ' ');
-	while (index)
+	index = 0;
+	vazio = NULL;
+	printf("%s", "TESTE 1: \tNULL\tft_split(teste, ' ')\n");
+	print = ft_split(vazio, ' ');
+	printf("%s", "\nOUT OF THE SPLIT\n");
+	if (print == NULL)
+		printf("%s", "\nNULL\n\n");
+	printf("%s", "\n--------------------------------------------------\nTESTE 2: \tpost tenebrvs lux\tft_split(teste, ' ')\n");
+	teste = "post	tenebrvs	lux";
+	print = ft_split(teste, ' ');
+	printf("%s", "\nTEST 2 IS SPLITTED\n");
+	if (print == NULL)
+		printf("%s", "\nI'M STILL NULL\n\n");
+	else
+		printf("%s", "\nTEST 2 CHANGED SOMETHING\n\n");
+	if (print[index] == teste)
+		printf("%s", "\nTEST 2 WORKED\n\n");
+	else
+		{
+			printf("%s", "TEST 2 DIDN'T WORK AT ALL");
+			return (0);
+		}
+	printf("%s", "\nTEST 2 WORKED\n\n");
+	printf("%s", "\n--------------------------------------------------\nTESTE 3: \tpost tenebrvs lux\tft_split(teste, ' ')\n");
+	teste = "post tenebrvs lux";
+	print = ft_split(teste, ' ');
+	index = 0;
+	while (print[index])
 	{
-		printf("%s", print[index]);
+		printf("%s\n", print[index]);
 		index++;
 	}
+	return (0);
 }
