66fe8d8940eb79e3352f406d469a606e4662f5fd
= <=>
2023-05-29 19:40:36 +0100

    Feat: added tester for lstmap & lstnew

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -6,11 +6,17 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:09:56 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/05/11 16:22:48 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/05/29 19:39:35 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 t_list	*ft_lstnew(void *content)
 {
@@ -22,4 +28,19 @@ t_list	*ft_lstnew(void *content)
 	lst->content = content;
 	lst->next = NULL;
 	return (lst);
+}
+
+int	main(void)
+{
+	t_list	*head;
+	t_list	*second;
+	t_list	*third;
+
+	head->content = 1;
+	head->next = second;
+	second->content = 2;
+	head->next = third;
+	third->content = 3;
+	head->next = NULL;
+	printList(ft_lstnew(head));
 }
