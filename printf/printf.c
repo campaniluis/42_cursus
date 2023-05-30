825708a70b76e062bc700f41008b6ad27d33dfa6
= <=>
2023-05-30 12:33:55 +0100

    Start printf | Feat gnl: substr, read_f, find_nl, gnl.c

--- printf/printf.c
+++ printf/printf.c
@@ -1,25 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   printf.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/30 12:12:11 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/05/30 12:32:30 by lclaudio         ###   ########.fr       */
+/*   Created: 2023/05/30 12:11:58 by lclaudio          #+#    #+#             */
+/*   Updated: 2023/05/30 12:13:22 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libprintf.h"
 
-char	*get_next_line(int fd);
-{
-	char	*next_line;
-	char	*str;
-	size_t	counter;
-
-	str = read_file();
-	counter = 0;
-	next_line = ft_substr(str, counter, (counter + find_newline(str, )));
-	return (next_line);
-}
