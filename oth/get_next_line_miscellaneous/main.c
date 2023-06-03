3b1cfa6b61ca46c6b85c3c06a9f71b3e48290b20
= <=>
2023-06-03 12:53:10 +0100

    Feat oth folder to store comments + testers

---
+++ oth/get_next_line_miscellaneous/main.c
@@ -0,0 +1,34 @@
+#include "get_next_line.h"
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+
+	fd = open("text.txt", O_RDONLY);
+	line = get_next_line(fd);
+		printf("line 1 content:\n%s\n", line);
+    free(line);
+	printf("------------------------------------------\n\nReading second line:\n");
+		line = get_next_line(fd);
+	if (line)
+		printf("\nline 2 content:\n%s\n", line);
+	else
+		printf("NULL\n");
+	free(line);
+	printf("------------------------------------------\n\nReading third line:\n");
+	line = get_next_line(fd);
+	if (line)
+		printf("\nThird line: \n%s\n", line);
+	else
+		printf("NULL\n");
+	free(line);
+	printf("------------------------------------------\n\nReading empty line:\n");
+	line = get_next_line(fd);
+	if (line == NULL)
+		printf("\nCONGRATS! YOU'VE READ A FILE\n");
+	else
+		printf("\nSomething's wrong in the end: \n%s\n", line);
+	free(line);
+	return (0);
+}
