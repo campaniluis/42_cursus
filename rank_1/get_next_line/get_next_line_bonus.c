ab55a0fc8c5f517f702aca2de2eb560a2191d0f1
= <=>
2023-06-03 17:52:45 +0100

    Mem leak + added int main

--- rank_1/get_next_line/get_next_line_bonus.c
+++ rank_1/get_next_line/get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:12:11 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/06/02 22:02:35 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/06/03 17:45:17 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ size_t	line_size(char *str)
 	return (index);
 }
 
-char	*get_line(char *str)
+char	*get_a_line(char *str)
 {
 	int		index;
 	char	*line;
 
-	line = malloc(sizeof(char) * (line_size(str) + 1)); // III ends there here
+	line = malloc(sizeof(char) * (line_size(str) + 1));
 	if (!line)
 		return (NULL);
 	index = 0;
@@ -68,12 +68,12 @@ char	*complete_line(int fd, char *buff)
 	char	*line;
 	size_t	index;
 
-	line = get_line(buff);
+	line = get_a_line(buff);
 	while (!ft_strchr(buff, '\n') && buff[0] != '\0')
 	{
 		index = read(fd, buff, BUFFER_SIZE);
 		buff[index] = 0;
-		line = ft_strjoin(line, get_line(buff)); // II goes here
+		line = ft_strjoin(line, get_a_line(buff));
 	}
 	buff_clean(buff);
 	return (line);
@@ -81,22 +81,71 @@ char	*complete_line(int fd, char *buff)
 
 char	*get_next_line(int fd)
 {
-	static char		buff[BUFFER_SIZE + 1];
+	static char		buff[4096][BUFFER_SIZE + 1];
 	char			*line;
 	size_t			index;
 
 	if (read(fd, 0, 0) < 0)
 	{
-		buff[0] = 0;
+		buff[fd][0] = 0;
 		return (NULL);
 	}
-	if (!buff[0])
+	if (!buff[fd][0])
 	{
-		index = read(fd, buff, BUFFER_SIZE);
-		buff[index] = 0;
+		index = read(fd, buff[fd], BUFFER_SIZE);
+		buff[fd][index] = 0;
 		if (index == 0)
 			return (NULL);
 	}
-	line = complete_line(fd, buff); // I starts here
+	line = complete_line(fd, buff[fd]);
 	return (line);
 }
+
+// bit.ly/43F9S6B
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		dialogue_1;
+// 	int		dialogue_2;
+// 	int		index;
+// 	int		counter;
+
+// 	dialogue_1 = open("dialogue_1.txt", O_RDONLY);
+// 	dialogue_2 = open("dialogue_2.txt", O_RDONLY);
+// 	index = 0;
+// 	while (index < 2)
+// 	{
+// 		counter = 0;
+// 		while (counter < 2)
+// 		{
+// 			line = get_next_line(dialogue_1);
+// 			printf("Troy:	%s\nCurrent fd number: %d\n", line, dialogue_1);
+// 			free(line);
+// 			counter++;
+// 		}
+// 		counter = 0;
+// 		while (counter < 2)
+// 		{
+// 			line = get_next_line(dialogue_2);
+// 			printf("Ahmed:	%sCurrent fd number: %d\n\n", line, dialogue_2);
+// 			free(line);
+// 			counter++;
+// 		}
+// 		index++;
+// 	}
+// 	index = 0;
+// 	while (index < 2)
+// 	{
+// 		line = get_next_line(dialogue_2);
+// 		printf("Ahmed:	%sCurrent fd number: %d\n\n", line, dialogue_2);
+// 		free(line);
+// 		line = get_next_line(dialogue_1);
+// 		printf("Troy:	%sCurrent fd number: %d\n\n", line, dialogue_1);
+// 		free(line);
+// 		index++;
+// 	}
+// 	printf("END OF SCENE\n");
+// 	close(dialogue_1);
+// 	close(dialogue_2);
+// 	return (0);
+// }
