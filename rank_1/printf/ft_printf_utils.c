fbb23c0a236b48747f98368163464d609a53db05
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-06-04 23:58:10 +0000

    Rm all from this file, just prototype remains

--- rank_1/printf/ft_printf_utils.c
+++ rank_1/printf/ft_printf_utils.c
@@ -12,41 +12,17 @@
 
 #include "libftprintf.h"
 
-int	ft_isalpha(int c)
-{	
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
-}
 
-char	trigger(const char format, char *flags)
-{
-	int counter;
+// char	trigger(const char format, char *flags)
+// {
+// 	int counter;
 
-	counter = 0;
-	while (flags[counter] != '\0' && format != flags[counter])
-	{
-		if (format == flags[counter])
-			return (flags[counter]);
-		counter++;
-	}
-	return (NULL);
-}
-
-char	*ft_strchr(const char *str, int to_find)
-{
-	int	index;
-
-	index = 0;
-	while (str[index] != '\0')
-	{
-		if (str[index] == (unsigned char)to_find)
-			return ((char *)&str[index]);
-		index++;
-	}
-	if (str[index] == (unsigned char)to_find)
-		return ((char *)&str[index]);
-	return (NULL);
-}
+// 	counter = 0;
+// 	while (flags[counter] != '\0' && format != flags[counter])
+// 	{
+// 		if (format == flags[counter])
+// 			return (flags[counter]);
+// 		counter++;
+// 	}
+// 	return (NULL);
+// }
