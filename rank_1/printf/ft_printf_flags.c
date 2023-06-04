20b382ea696cae703a502a5318d8304b3754b2fa
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-06-04 23:57:22 +0000

    Feat bonus_point

--- rank_1/printf/ft_printf_flags.c
+++ rank_1/printf/ft_printf_flags.c
@@ -1,11 +1,35 @@
-// 
-required(const char *format, va_list args, char flag)
-{
+#include "libftprintf.h"
 
+// 
+void	required(char c)
+{
+    printf("required: %c\n", c);
 }
 
-// 
-bonus(const char *format, va_list args, char flag)
+int bonus_point(const char *format, char c, size_t index)
+{
+	char	*str_size;
+	size_t	len;
+	size_t	counter;
+
+	index++;
+	counter = 0;
+	while(ft_isdigit(format[index + counter]))
+		counter++;
+	str_size = malloc(sizeof(char) * (counter + 1));
+	str_size = ft_substr(format, index, counter);
+	len = ft_atoi(str_size);
+	printf("HERE size: %zu\n", len);
+	index += counter;
+	free(str_size);
+	return (len);
+}
+
+//
+void	bonus(const char *format, char c, size_t index)
 {
 
+// especifica o n de chars a ler do argumento
+    if (c == '.')
+        bonus_point(format, c, index);
 }
