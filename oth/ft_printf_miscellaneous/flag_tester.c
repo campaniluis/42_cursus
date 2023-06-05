ef5e60849e97b2e001f9ec1e1e27de9722efc9df
Luís Campani Farias <105250701+campaniluis@users.noreply.github.com>
2023-06-05 00:01:40 +0000

    Mv printf miscellaneous here

---
+++ oth/ft_printf_miscellaneous/flag_tester.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// -0.# +
+
+int main(void)
+{
+    // int index;
+    const char    *str = "Hello World!\n";
+
+printf( "Hello World!\n");
+
+printf("    Hello World!\n");
+
+printf(     "Hello World!\n");
+printf("str");
+
+
+    // index = printf("Hello World!\n");
+    // printf("printf returned: %-d\n", index);
+    return (0);
+}
