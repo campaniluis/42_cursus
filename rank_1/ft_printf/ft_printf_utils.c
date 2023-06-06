cb2cfa673bb92242eb371b8d12e9c0c2373f70a7
= <=>
2023-06-06 11:39:48 +0100

    Added not_alpha_len

--- rank_1/ft_printf/ft_printf_utils.c
+++ rank_1/ft_printf/ft_printf_utils.c
@@ -6,12 +6,21 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:11:58 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/06/05 08:57:10 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/06/06 11:39:31 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
+size_t	not_alpha_len(const char *str, size_t index)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (str[index + counter] != '\0' && !ft_isalpha(str[index + counter]))
+		counter++;
+	return (counter);
+}
 
 int	trigger(const char c, char *flags)
 {
