ead6c1799a8e5cc220d9fe5f4e26001e8863586d
= <=>
2023-06-06 11:40:29 +0100

    Added flag_reader

--- rank_1/ft_printf/ft_printf_flags.c
+++ rank_1/ft_printf/ft_printf_flags.c
@@ -1,5 +1,27 @@
 #include "libftprintf.h"
 
+// char	*flag_reader(const char *format, size_t index, va_list args)
+char	*flag_reader(const char *format, size_t index)
+{
+	char	*temp;
+	size_t	temp_len;
+	size_t		counter;
+
+	temp_len = not_alpha_len(format, index);
+	temp = malloc(temp_len * sizeof(char));
+	counter = 0;
+	while(counter < temp_len)
+	{
+		temp[counter] = format[counter + index];
+		counter++;
+	}
+	// se for um char da struct
+	
+	// voce vai pegar a struct
+	// ver se eh asterisco
+	// e salvar o valor
+	return(temp);
+}
 
 char	*required(char c, va_list args)
 {
@@ -50,15 +72,19 @@ int	bonus_zero(const char *format, size_t index, va_list args)
 	index++;
 	blanks = '0';
 	counter = 0;
-	if (trigger(format[index], "sdiuxX"))
+	if (format[index] == '*')
 	{
-		blanks_size = va_arg(args, int);
-		next_arg = required(format[index], args);
-		size_next_arg = ft_strlen(next_arg);
-		while (counter++ < blanks_size - size_next_arg)
-			write(1, &blanks, 1);
-		write(1, &next_arg[0], size_next_arg);
-		return (blanks_size);
+		index++;
+		if (trigger(format[index], "sdiuxX"))
+		{
+			blanks_size = va_arg(args, int);
+			next_arg = required(format[index], args);
+			size_next_arg = ft_strlen(next_arg);
+			while (counter++ < blanks_size - size_next_arg)
+				write(1, &blanks, 1);
+			write(1, &next_arg[0], size_next_arg);
+			return (blanks_size);
+		}
 	}
 	return (0);
 }
