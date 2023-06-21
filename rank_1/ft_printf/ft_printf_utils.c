69b744554c1c5f09d75982b53dd576ab922874ae
= <=>
2023-06-21 21:21:49 +0100

    rm structs branch + rm all struct related functions

--- rank_1/ft_printf/ft_printf_utils.c
+++ rank_1/ft_printf/ft_printf_utils.c
@@ -6,60 +6,13 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:11:58 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/06/06 11:39:31 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/06/21 21:20:53 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	numerus(const char* format, size_t index)
-{
-	char	*specif_str;
-	int		specif_arg;
-	size_t	counter;
-	size_t	len;
-
-	if (!ft_isdigit(format[index]))
-		return(0);
-	len = 0;
-	while (ft_isdigit(format[index + len]))
-		len++;
-	specif_str = malloc(sizeof(char) * len);
-	counter = 0;
-	while(counter < len)
-	{
-		specif_str[counter] = format[counter + index];
-		counter++;
-	}
-	specif_arg = ft_atoi(specif_str);
-	return(specif_arg);
-}
-
-int	asteriskos(const char* format, size_t index, va_list args)
-{
-	int	specif_arg;
-	if (format[index] == '*')
-	{
-		specif_arg = va_arg(args, int);
-		return(specif_arg);
-	}
-	return(0);
-}
-
-int	flag_size_finder(const char *temp, size_t  index, va_list args)
-{
-	int	size;
-
-    if (ft_isdigit(temp[index]))
-		size = numerus(temp, index);
-	else if (temp[index] == '*')
-        size = asteriskos(temp, index, args);
-	else
-		size = 0;
-    return(size);
-}
-
-void	write_argument(const char *arg, size_t arg_size)
+int	write_argument(const char *arg, size_t arg_size)
 {
 	size_t	counter;
 
@@ -69,28 +22,5 @@ void	write_argument(const char *arg, size_t arg_size)
 		write(1, &arg[counter], 1);
 		counter++;
 	}
-}
-
-size_t	not_alpha_len(const char *str, size_t index)
-{
-	size_t	counter;
-
-	counter = 0;
-	while (str[index + counter] != '\0' && !ft_isalpha(str[index + counter]))
-		counter++;
 	return (counter);
-}
-
-int	trigger(const char c, char *flags)
-{
-	int	counter;
-
-	counter = 0;
-	while (flags[counter] != '\0')
-	{
-		if (c == flags[counter])
-			return (1);
-		counter++;
-	}
-	return (0);
 }
--- rank_1/ft_printf/libftprintf.h
+++ rank_1/ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: lclaudio <lclaudio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:11:51 by lclaudio          #+#    #+#             */
-/*   Updated: 2023/06/06 11:41:38 by lclaudio         ###   ########.fr       */
+/*   Updated: 2023/06/21 21:21:23 by lclaudio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,6 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdarg.h>
-
-typedef struct s_flags
-{
-	int	zero;
-	int	minus;
-	int	point;
-	int	hashtag;
-	int	plus;
-	int	space;
-}t_flags;
 
 // libft functions
 int		ft_atoi(const char *str);
@@ -43,21 +33,6 @@ char	*ft_strdup(char *str);
 // printf.c
 int		ft_printf(const char *, ...);
 // printf_utils.c
-int		asteriskos(const char* format, size_t index, va_list args);
-int		numerus(const char* format, size_t index);
-int	flag_size_finder(const char *temp, size_t  index, va_list args);
-size_t	not_alpha_len(const char *str, size_t index);
-int		trigger(const char c, char *flags);
-void	write_argument(const char *arg, size_t arg_size);
+int		write_argument(const char *arg, size_t arg_size);
 
-// ft_printf_flags.c
-char	*flag_reader(const char *format, size_t index);
-char	*required(char c, va_list args);
-int		bonus_minus(const char *format, size_t index, va_list args);
-int		bonus_zero(const char *format, size_t index, va_list args);
-int		bonus_point(const char *format, size_t index, va_list args);
-// int 	bonus_hashtag(const char *format, size_t index, va_list args);
-int		bonus_space(const char *format, size_t index, va_list args);
-int		bonus_plus(const char *format, size_t index, va_list args);
-int		bonus(const char *format, char c, size_t index, va_list args);
 #endif
